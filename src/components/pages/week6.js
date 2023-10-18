7746d40eabe851cfc1a0140c9b71dc3a88ae327c
LeeJAJA <jiajie@mit.edu>
2023-10-18 13:30:06 -0400

    Updated week6

--- src/components/pages/week6.js
+++ src/components/pages/week6.js
@@ -19,13 +19,26 @@ const Content = () => (
     <div style={{ width: "100%", marginBottom: "40px" }} alt={""}>
       <img className={"img_aspect"} src={`/assets/week6/3.jpg`} alt={""} />
     </div>
+    <div className={"page_content_subtitle"}>Calibration</div>
+    <div className={"page_content_content"}>
+      <ReactPlayer url="https://youtu.be/jTWsOtTFGgc" />
+    </div>
     <div className={"page_content_subtitle"}>Trace Cutting</div>
     <div style={{ width: "100%", marginBottom: "40px" }} alt={""}>
       <img className={"img_aspect"} src={`/assets/week6/4.jpg`} alt={""} />
     </div>
+    <div className={"page_content_content"}>
+      <ReactPlayer url="https://youtu.be/pFla8mT1rXs" />
+    </div>
+    <div className={"page_content_content"}>
+      <ReactPlayer url="https://youtu.be/nMDMf4axEoI" />
+    </div>
     <div className={"page_content_subtitle"}>Interior Cutting</div>
     <div style={{ width: "100%", marginBottom: "40px" }} alt={""}>
       <img className={"img_aspect"} src={`/assets/week6/5.jpg`} alt={""} />
+    </div>
+    <div className={"page_content_content"}>
+      <ReactPlayer url="https://youtu.be/IYQraGwEzXY" />
     </div>
     <div className={"page_content_subtitle"}>Soldering</div>
     <div style={{ width: "100%", marginBottom: "40px" }} alt={""}>
@@ -38,12 +51,20 @@ const Content = () => (
     <div style={{ width: "100%", marginBottom: "40px" }} alt={""}>
       <img className={"img_aspect"} src={`/assets/week6/8.jpg`} alt={""} />
     </div>
-
+    <div className={"page_content_subtitle"}>Final Product</div>
+    <div className={"page_content_content"}>
+      <ReactPlayer url="https://youtube.com/shorts/qZ4hjk0ZMHk" />
+    </div>
   </>
 );
 
 const Page = () => {
-  return <ContentTemplate title="Week6 Electronics Production" content={<Content />} />;
+  return (
+    <ContentTemplate
+      title="Week6 Electronics Production"
+      content={<Content />}
+    />
+  );
 };
 
 export default Page;
